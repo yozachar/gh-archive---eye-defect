6a2119962622d909392a36ac443898d6b1cb6d9c
Jovial Joe Jayarson <jovial7joe@hotmail.com>
2020-10-21 21:47:03 +0530

    feat: initial commit; adds todo

---
+++ src/main.py
@@ -0,0 +1,6 @@
+# Todo
+# 1. Collect images
+# 2. Extract data
+# 3. Go with second method - 3 folders R, B, & G
+# Open all the folders together - Extract the colors and drop them seperately
+# Finally resize them
