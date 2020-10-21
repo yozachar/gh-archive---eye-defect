c356e51088a1804465d3a20f911418930c53bf21
Jovial Joe Jayarson <jovial7joe@hotmail.com>
2020-10-21 21:48:00 +0530

    feat: passes primary test

--- src/main.py
+++ src/main.py
@@ -4,3 +4,19 @@
 # 3. Go with second method - 3 folders R, B, & G
 # Open all the folders together - Extract the colors and drop them seperately
 # Finally resize them
+
+from PIL import Image
+
+img = Image.open('test_in.tif')
+data = img.getdata()
+
+r = [(d[0], 0, 0) for d in data]
+g = [(0, d[1], 0) for d in data]
+b = [(0, 0, d[2]) for d in data]
+
+img.putdata(r)
+img.save('r.tif')
+img.putdata(g)
+img.save('g.tif')
+img.putdata(b)
+img.save('b.tif')
